fa78eba546afde8b47347856c6e9cc588ac705a3
Vinicius <viniciusnevez123@gmail.com>
2026-09-24 17:27:35 -0300

    Bump Supertexto cache to v5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_CACHE='supertexto-app-v4',RUNTIME_CACHE='supertexto-runtime-v4';
+const APP_CACHE='supertexto-app-v5',RUNTIME_CACHE='supertexto-runtime-v5';
 const SHELL=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(APP_CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>![APP_CACHE,RUNTIME_CACHE].includes(k)).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
